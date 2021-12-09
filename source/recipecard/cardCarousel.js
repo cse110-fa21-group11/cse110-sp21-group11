40295eba3a9b339cbfbd8dcc8bf605026e341d6d
Donovan-Eral <donovaneral@gmail.com>
2021-12-08 17:35:49 -0800

    Made the recipe carousels resize on the screen size

--- source/recipecard/cardCarousel.js
+++ source/recipecard/cardCarousel.js
@@ -1,51 +1,137 @@
-// CardCarousel.js
 
 class CardCarousel extends HTMLElement {
     constructor() {
         super();
         this.attachShadow({ mode: 'open' });
-    }
 
+        const style = document.createElement('style');
+        style.innerHTML = `
+        .back, .forward {
+            cursor: pointer;
+            position: relative;
+            top: 50%;
+            display: block;
+            width: auto;
+            height: 20px;
+            padding: 17px;
+            border-radius: 50px;
+            color: black;
+            font-size: 20px;
+            overflow: hidden;
+            transition: all .35s;
+          }
+          
+          .back {
+          }
+          
+          /* place forward button to right of page */
+          .forward {
+          }
+          
+          /* button transition features */
+          .back:hover, .forward:hover {
+            color: #df342f;
+          }
+          
+          .back:before, .back:after,
+          .forward:before, .forward:after {  
+            position: absolute;
+            content: "";
+            width: 100%;
+            height: 100%;
+            top: -100%;
+            left: 0;
+            background: lightgrey;
+            z-index: -1;
+            transition: all .35s;
+          }
+          
+          .back:before, .forward:before {
+            opacity: .5;
+          }
+          
+          .back:after, .forward:after {
+            transition-delay: .25s;
+          }
+          
+          .back:hover:before, .back:hover:after,
+          .forward:hover:before, .forward:hover:after {
+            top: 0;
+          }
+          `;
 
-    /*
-    * This function will be called in an event listener when the right button is clicked.
-    * The next 3 recipe cards will be shown.
-    */
-    nextCards() {
-        const cards = this.shadowRoot.querySelectorAll('recipe-card');
-        for (let i = cards.length - 4; i >= 0; i--) {
-            if (cards[i].getAttribute('style') != 'display: none') {
-                cards[i].setAttribute('style', 'display: none');
-                cards[i + 3].style.display = '';
-            }
-        }
-    }
-
-    /*
-    * This function will be called in an event listener when the left button is clicked.
-    * The previous 3 recipe cards will be shown.
-    */
-    prevCards() {
-        const cards = this.shadowRoot.querySelectorAll('recipe-card');
-        for (let i = 3; i < cards.length; i++) {
-            if (cards[i].getAttribute('style') != 'display: none') {
-                cards[i].setAttribute('style', 'display: none');
-                cards[i - 3].style.display = '';
-            }
-        }
+        this.shadowRoot.append(style);
     }
 
 
     // paramter is in an array of recipe cards
     createCardCarousel(recipeCards) {
+        const forwardButton = document.createElement('a');
+        const backwardButton = document.createElement('a');
+        forwardButton.classList.add('forward')
+        backwardButton.classList.add('back')
+        forwardButton.innerHTML = '&#10095;';
+        backwardButton.innerHTML = '&#10094;';
 
+        // Add the back button to the card carousel
+        this.shadowRoot.append(backwardButton);
+        //this.shadowRoot.querySelector('.back').setAttribute('style', 'display: none');
+
+        // Set cards in the carousel
         for (let i = 0; i < recipeCards.length; i++) {
-            if (i > 2) {
-                recipeCards[i].setAttribute('style', 'display: none');
-            }
             this.shadowRoot.append(recipeCards[i]);
         }
 
+        var currentIndex = 0;
+        // gets a number of recipes that would fit on the screen
+        var numberOfrecipes = parseInt(window.innerWidth / 400);
+
+        this.displayCards(currentIndex, numberOfrecipes, recipeCards);
+
+        // Add the forward button to the card carousel
+        this.shadowRoot.append(forwardButton);
+
+        //Binds the back and forward buttons to their respective carousel's functions
+        this.shadowRoot.querySelector('.back').addEventListener('click', () => {
+            this.displayCards(currentIndex, numberOfrecipes, recipeCards);
+            currentIndex -= numberOfrecipes;
+            if (currentIndex < 0) {
+                currentIndex = 0;
+                // Since there is nothing to go back to, we need to indicate that the carousel is at the end
+                //this.shadowRoot.querySelector('.back').setAttribute('style', 'display: none');
+            }
+            //this.shadowRoot.querySelector('.forward').setAttribute('style', '');
+        });
+
+
+        this.shadowRoot.querySelector('.forward').addEventListener('click', () => {
+            this.displayCards(currentIndex, numberOfrecipes, recipeCards);
+            currentIndex += numberOfrecipes;
+            if (currentIndex > recipeCards.length - numberOfrecipes) {
+                currentIndex = recipeCards.length - numberOfrecipes;
+                // Since there is nothing to go forward to, we need to indicate that the carousel is at the end
+                //this.shadowRoot.querySelector('.forward').setAttribute('style', 'display: none');
+            }
+            //this.shadowRoot.querySelector('.back').setAttribute('style', '');
+        });
+
+        // Adjusts the number of cards displayed when the window is resized
+        window.addEventListener('resize', () => {
+            // gets a number of recipes that would fit on the screen
+            numberOfrecipes = parseInt(window.innerWidth / 400);
+            this.displayCards(currentIndex, numberOfrecipes, recipeCards);
+        });
+    }
+
+    // display the the number of cards specified by the parameter starting from an initial index
+    displayCards(startingIndex, numberOfrecipes, recipeCards) {
+        for (let i = 0; i < recipeCards.length; i++) {
+            if (startingIndex <= i && i < startingIndex + numberOfrecipes) {
+                recipeCards[i].setAttribute('style', '');
+            } else {
+                recipeCards[i].setAttribute('style', 'display: none');
+            }
+        }
     }
 
     
